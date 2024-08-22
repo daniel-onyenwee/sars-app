fb029e283a7ceb3759807daff0ce317d592a8d89
daniel-onyenwee <danielonyenwee@gmail.com>
2024-08-22 11:08:50 +0100

    ➕ add global error page

---
+++ app/webapp/src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { Button } from "@/components/ui/button";
+  import { page } from "$app/stores";
+</script>
+
+<svelte:head>
+  <title>
+    {$page.status} - {$page.error?.message}
+  </title>
+</svelte:head>
+
+<main
+  class="bg-muted/40 h-screen w-screen flex justify-center flex-col items-center gap-4"
+>
+  <p class="text-muted-foreground text-7xl">Oops!</p>
+  <p class="text-muted-foreground mt-2 text-lg">
+    {$page.status} - {$page.error?.message}
+  </p>
+  <Button href="/">GO TO HOMEPAGE</Button>
+</main>

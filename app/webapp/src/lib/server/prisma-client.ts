00edbbfa565403a0da4d790949bca5bd2361efc0
daniel-onyenwee <danielonyenwee@gmail.com>
2024-08-29 22:38:55 +0100

    🛠️ fix sveltekit build error -> __dirname is undefined

--- app/webapp/src/lib/server/prisma-client.ts
+++ app/webapp/src/lib/server/prisma-client.ts
@@ -1,4 +1,11 @@
-import { PrismaClient } from "@prisma/client"
 import { env } from "$env/dynamic/private"
+import type { PrismaClient as PrismaClientType } from "@prisma/client"
+import { createRequire } from "module"
+
+const require = createRequire(import.meta.url ?? __filename);
+
+const { PrismaClient: PrismaClientImpl } = require('@prisma/client');
+
+class PrismaClient extends (PrismaClientImpl as typeof PrismaClientType) { }
 
 export const prismaClient = new PrismaClient({ datasourceUrl: env.DATABASE_URL })

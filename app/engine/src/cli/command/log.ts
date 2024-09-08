4334f6362951c1242957dd504413f79773c68a4d
daniel-onyenwee <danielonyenwee@gmail.com>
2024-09-08 23:16:50 +0100

    ❌ remove unnecessary error handler

--- app/engine/src/cli/command/log.ts
+++ app/engine/src/cli/command/log.ts
@@ -1,9 +1,10 @@
 import { Command } from "@commander-js/extra-typings"
 import Logger from "@sars/logger"
 import { existsSync, readFileSync } from "node:fs"
+import { extname } from "node:path"
 
-const logAction: Parameters<typeof logCommand.action>[0] = async function ({ dataUrl, loggerId, output, apply }) {
-    let logger = new Logger(loggerId, dataUrl)
+const logAction: Parameters<typeof logCommand.action>[0] = async function ({ databaseUrl, loggerId, output, apply }) {
+    let logger = new Logger(loggerId, databaseUrl)
 
     if (output) {
         await logger.output("data", output)
@@ -25,41 +26,26 @@ const logAction: Parameters<typeof logCommand.action>[0] = async function ({ dat
 
     try {
         logData = JSON.parse(readFileSync(apply, "utf8"))
+        if (extname(apply) != ".log") {
+            throw new Error()
+        }
     } catch {
         console.log("error: invalid log file found")
         return
     }
 
     try {
-        console.log("Restoring system state")
         await logger.checkout()
+        await logger.apply(logData)
+        console.log("Successfully applied log changes")
     } catch (error) {
-        console.log("error: system state restoration failed")
-        process.exit()
-    }
-
-    try {
-        try {
-            try {
-                await logger.apply(logData)
-                console.log("Successfully applied log changes")
-            } catch (error) {
-                console.log("error: log changes failed to apply")
-                throw error
-            }
-        } catch (error) {
-            console.log("Restoring system state")
-            await logger.checkout()
-        }
-    } catch (error) {
-        console.log("error: system state restoration failed")
-        process.exit()
+        console.log("error: log changes failed to apply")
     }
 }
 
 export const logCommand = new Command("log")
     .description("Show, apply and output the SARs engine log changes.")
-    .requiredOption("-d, --data-url <url>", "SARs postgres database url.")
+    .requiredOption("-d, --database-url <url>", "SARs postgres database url.")
     .requiredOption("-i, --logger-id <id>", "Unique identification of user.")
     .option("-e, --entries", "Show all the log entries.")
     .option("-o, --output <path>", "Save log changes to path.")

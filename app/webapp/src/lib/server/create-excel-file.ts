7133beefad107977143f8aefcc6bb6fc5b17bd70
daniel-onyenwee <danielonyenwee@gmail.com>
2024-09-16 02:32:15 +0100

    🔄️ refactor utility function -> createExcelFile

--- app/webapp/src/lib/server/create-excel-file.ts
+++ app/webapp/src/lib/server/create-excel-file.ts
@@ -1,4 +1,4 @@
-import ExcelJS from "exceljs"
+import * as XLSX from "xlsx"
 
 interface ExcelProperty {
     creator: string
@@ -12,21 +12,32 @@ interface ExcelProperty {
     }[]
 }
 
-export async function createExcelFile(property: ExcelProperty, data: any[]): Promise<Buffer> {
-    const workbook = new ExcelJS.Workbook()
-    workbook.creator = property.creator
-    workbook.title = property.title
-    workbook.created = property.created
+export function createExcelFile(property: ExcelProperty, data: Record<string, any>[]): Buffer {
+    const workbook = XLSX.utils.book_new()
 
-    const workSheet = workbook.addWorksheet("Sheet 1", {
-        headerFooter: {
-            firstHeader: property.firstHeader
+    const columnsMap = new Map(property.columns.map(({ key, width, ...otherData }) => [key, { width: width || 10, ...otherData }]))
+
+    data = data.map((row) => {
+        const newRow: Record<string, any> = {};
+
+        for (const key of columnsMap.keys()) {
+            const columnData = columnsMap.get(key);
+
+            if (columnData && row[key] != undefined) {
+                newRow[columnData.header] = row[key];
+            }
         }
-    })
 
-    workSheet.columns = property.columns
+        return newRow;
+    });
 
-    workSheet.addRows(data)
+    const sheet = XLSX.utils.json_to_sheet(data)
 
-    return Buffer.from(await workbook.xlsx.writeBuffer())
+    sheet["!cols"] = Array.from(columnsMap.values()).map(({ width }) => ({ width }))
+
+    XLSX.utils.book_append_sheet(workbook, sheet, "Sheet1")
+
+    const workbookBuffer = XLSX.write(workbook, { type: "buffer", Props: { CreatedDate: property.created, Author: property.creator, Title: property.title } })
+
+    return Buffer.from(workbookBuffer)
 }

d99ea147d21e74624d56bc7fa1f1a6e916f0b998
daniel-onyenwee <danielonyenwee@gmail.com>
2024-09-16 02:38:22 +0100

    🔄️ refactor route -> login to  support request coming from an electron platform

--- app/webapp/src/routes/api/v1/auth/login/+server.ts
+++ app/webapp/src/routes/api/v1/auth/login/+server.ts
@@ -4,8 +4,14 @@ import { env } from "$env/dynamic/private"
 import { sign } from "@tsndr/cloudflare-worker-jwt"
 import { addDays } from "date-fns"
 
+interface LoginRequestBody {
+    password: string,
+    platform?: "Electron" | "Browser",
+    systemPassword?: string
+}
+
 export const POST: RequestHandler = async ({ request }) => {
-    let body: { password: string } | null = null
+    let body: LoginRequestBody | null = null
 
     try {
         body = await request.json()
@@ -35,7 +41,8 @@ export const POST: RequestHandler = async ({ request }) => {
         })
     }
 
-    if (body.password != env.ADMIN_PASSWORD) {
+    if ((body.password != env.ADMIN_PASSWORD && (!body.platform || body.platform == "Browser")) ||
+        (body.platform == "Electron" && body.password != body.systemPassword)) {
         return json.fail({
             ok: false,
             error: {
@@ -51,7 +58,7 @@ export const POST: RequestHandler = async ({ request }) => {
     let exp = Math.floor(expiresIn.getTime() / 1000) + (2 * (60 * 60))
 
     const accessToken = await sign({
-        password: body.password,
+        password: env.ADMIN_PASSWORD,
         exp
     }, env.ACCESS_TOKEN_SECRET)
 
